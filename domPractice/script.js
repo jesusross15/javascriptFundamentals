55f19644c5dfd7c0da29401de10d548463883c4a
Jesus Ross <91804598+jesusross15@users.noreply.github.com>
2023-09-15 15:26:34 -0400

    getting to know event listeners and named functions

--- domPractice/script.js
+++ domPractice/script.js
@@ -35,7 +35,29 @@ const anotherP = document.createElement('p');
 anotherP.classList.add('p');
 anotherP.textContent = "So am I";
 
-// appending the elelments to the div BEFORE appending the div to the container
+// appending the elements to the div BEFORE appending the div to the container
 box.appendChild(h1);
 box.appendChild(anotherP);
 container.appendChild(box);
+
+// Method 2
+// This is a little better bc we moved the JS out of the html file but we still have the problem
+// where we can only have 1 "onclick" property
+const btn2 = document.querySelector('#btn2');
+btn2.onclick = () => alert("Hello World");
+
+// Method 3
+// In this method we seperate the JS out of the html AND we can also allow multiple event listeners
+// if the need arises. a lot more flexible and powerful even though it's complex to set up
+const btn3 = document.querySelector('#btn3');
+btn3.addEventListener('click', () => {
+  alert("Hello World");
+});  
+// Can be simplified even more like this
+// btn3.addEventListener('click', () => alert("Hello World"));
+
+function alertFunction() {
+    btn.addEventListener('click', function (e) {
+        e.target.style.background = 'blue';
+      });
+  }
